0006ce398697dcce5c152e6a4265529abea7ef0b
kamataryo <mugil.cephalus+ciconia.local@gmail.com>
2018-09-07 12:19:05 +0900

    Some fix

--- index.js
+++ index.js
@@ -4,9 +4,8 @@ const request = require('request')
 http
   .createServer((req, res) => {
     const headers = { ...req.headers, 'accept-language': '' }
-    request('http://example.com', (err, _, body) => {
-      res.write(body.toString())
-      res.end(res.statusCode.toString())
+    request('http://example.com', (err, res2, body) => {
+      res.end(body.toString())
     })
   })
   .listen(3000)
